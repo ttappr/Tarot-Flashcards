b0923403dd23cf6c1d5e1febdfadfa6aabb81207
ttappr <tmtappr@gmail.com>
2020-12-27 17:07:41 -0800

    Decided to make this a "Progressive Web App" and forego using cordova - I'm not using native phone features in this app, so cordova doesn't make sense.

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,18 @@ const {CleanWebpackPlugin}      = require('clean-webpack-plugin');
 const path                      = require('path');
 const autoprefixer              = require('autoprefixer');
 const CopyWebpackPlugin         = require('copy-webpack-plugin');
+const SWPrecacheWebpackPlugin   = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest        = require('webpack-pwa-manifest');
+
+const PUBLIC_PATH = 'https://ttappr.github.io/Tarot-Flashcards/';
+//const PUBLIC_PATH = 'http://localhost:8000';
 
 module.exports = [{
     entry: './www/index.js',
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'platforms/webpack')
+      path: path.resolve(__dirname, 'platforms/webpack'),
+      publicPath: PUBLIC_PATH,
     },
     module: {
       rules: [
@@ -66,6 +72,32 @@ module.exports = [{
                 {from: 'www/img', to: 'img'},
             ],
         }),
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'Tarot-Flashcards',
+            dontCacheBustUrlsMatching: /\.\w{8}\./,
+            filename: 'service-worker.js',
+            minify: true,
+            navigateFallback: PUBLIC_PATH + 'index.html',
+            staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+        }),
+        new WebpackPwaManifest({
+            name: 'Tarot Flashcards',
+            short_name: 'FlashTarot',
+            description: 'A simple flashcard app for memorizing Tarot card meanings.',
+            display: 'fullscreen', // other options: minimal-ui, fullscreen, standalone, browser.
+            orientation: 'portrait-primary',
+            background_color: '#01579b',
+            theme_color: '#01579b',
+            'theme-color': '#01579b',
+            start_url: '/',
+            icons: [
+              {
+                src: path.resolve('www/img/sun.jpeg'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons')
+              }
+            ]
+          })
     ],
     devServer: {
         contentBase: path.join(__dirname, 'www'), //'platforms/webpack'),
--- www/index.js
+++ www/index.js
@@ -9,25 +9,16 @@ import favicon          from './img/sun.jpeg';
 import index            from './html/index-body.html';
 import scss             from './css/index.scss';
 
-import fscreen from 'fscreen';
-
 import './js/nav-bar.js';
 import './js/tab-set.js';
 import './js/card-deck.js';
 import './js/coach.js';
 import './js/app-options.js';
 
-
-let m1 = ecreate('meta', {   name: 'apple-mobile-web-app-capable', 
-                          content: 'yes'});
-let m2 = ecreate('meta', {   name: 'apple-mobile-web-app-status-bar-style',
-                          content: 'black-translucent'});
-
-meappend(query('head'), [m1, m2]);
-
-let icon    = ecreate('link', { rel: 'shortcut icon', href: favicon,
-                               type: 'image/jpg',       id: 'favicon'});
-let styles  = ecreate('link', { rel: 'stylesheet',    href: scss});
+//let manifest = ecreate('link', { rel: 'manifest',      href: '/manifest.json'});
+let icon     = ecreate('link', { rel: 'shortcut icon', href: favicon,
+                                type: 'image/jpg',       id: 'favicon'});
+let styles   = ecreate('link', { rel: 'stylesheet',    href: scss});
 
 meappend(query('head'), [icon, styles]);
 
@@ -38,14 +29,17 @@ let tabs = query('tab-set');
 query('nav-bar').onclick = (e) => {
     if (e.target.hash) {
         let option = e.target.hash.slice(1);
-        if (option === 'fullscreen') {
-            if (!fscreen.fullscreenElement) {
-                fscreen.requestFullscreen(query('html'));
-            } else {
-                fscreen.exitFullscreen();
-            }
-        } else {
-            tabs.display(option);
-        }
+        tabs.display(option);
     }
 };
+
+(function () {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./service-worker.js', {
+                scope: '/Tarot-Flashcards/'
+            })
+            .then(() => console.log('Service Worker registered successfully.'))
+            .catch(error => console.log('Service Worker registration failed:', 
+                                        error));
+    }
+})();
